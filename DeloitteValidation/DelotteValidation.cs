feb2c19edb942927a9361534e20041fdab73746f
Noergaard <miknoergaard@deloitte.dk>
2018-08-30 22:22:30 +0200

    Regex Collection initial version finished

--- DeloitteValidation/DelotteValidation.cs
+++ DeloitteValidation/DelotteValidation.cs
@@ -26,12 +26,6 @@ namespace DeloitteValidation
         public MyEnum Option { get; set; }
 
         public Regex reg;
-        /*
-
-        [DefaultValue(null)]
-        public InArgument<string> Text { get; set; }
-
-        */
 
         [Category("Input")]
         [RequiredArgument]
@@ -40,7 +34,6 @@ namespace DeloitteValidation
         [Category("Output")]
         public OutArgument<bool> Result { get; set; }
 
-
         [Category("Output")]
         public OutArgument<string> ResultText { get; set; }
 
@@ -48,18 +41,81 @@ namespace DeloitteValidation
         {
 
             
-            
+           
             switch (string.Format("{0}",this.Option))
             {
                 case "Email":
-                    reg = new Regex(@"^\w{4}\d{6,7}$");
+                    reg = new Regex(@"\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*\s*$");
                     break;
 
-                case "OptionB":
-                    
+                case "Url":
+                    reg = new Regex(@"^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$");
+                    break;
+
+                case "Ip":
+                    reg = new Regex(@"^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$");
+                    break;
+
+                case "DatetimeDk":
+                    reg = new Regex(@"^(?=\d)(?:(?:31(?!.(?:0?[2469]|11))|(?:30|29)(?!.0?2)|29(?=.0?2.(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00)))(?:\x20|$))|(?:2[0-8]|1\d|0?[1-9]))([-./])(?:1[012]|0?[1-9])\1(?:1[6-9]|[2-9]\d)?\d\d(?:(?=\x20\d)\x20|$))?(((0?[1-9]|1[012])(:[0-5]\d){0,2}(\x20[AP]M))|([01]\d|2[0-3])(:[0-5]\d){1,2})?$");
+                    break;
+
+                case "Datetime":
+                    reg = new Regex(@"^(?=\d)(?:(?:(?:(?:(?:0?[13578]|1[02])(\/|-|\.)31)\1|(?:(?:0?[1,3-9]|1[0-2])(\/|-|\.)(?:29|30)\2))(?:(?:1[6-9]|[2-9]\d)?\d{2})|(?:0?2(\/|-|\.)29\3(?:(?:(?:1[6-9]|[2-9]\d)?(?:0[48]|[2468][048]|[13579][26])|(?:(?:16|[2468][048]|[3579][26])00))))|(?:(?:0?[1-9])|(?:1[0-2]))(\/|-|\.)(?:0?[1-9]|1\d|2[0-8])\4(?:(?:1[6-9]|[2-9]\d)?\d{2}))($|\ (?=\d)))?(((0?[1-9]|1[012])(:[0-5]\d){0,2}(\ [AP]M))|([01]\d|2[0-3])(:[0-5]\d){1,2})?$");
+                    break;
+                case "DaysoftheweekDk":
+                    reg = new Regex(@"^(Søn|søn|Man|man|(T(irs|ors))|(t(irs|ors))|Fre|fre)(dag|\.)?$|Ons|ons(\.|dag)?$|Lør|lør(\.|dag)?$|T((ir?)|(o?r?))|t((ir?)|(o?r?))\.?$");
+
+                    break;
+                case "Daysoftheweek":
+                    reg = new Regex(@"^(Sun|Mon|(T(ues|hurs))|Fri)(day|\.)?$|Wed(\.|nesday)?$|Sat(\.|urday)?$|T((ue?)|(hu?r?))\.?$");
+                    break;
+
+                case "MonthsDk":
+                    reg = new Regex(@"^(?:J|j(anuar|u(ni|li))|(Februar|februar)|Ma(rts|j)|ma(rts|j)|A(pril|ugust)|a(pril|ugust)|(((Sept|Nov|Dec)em)|Okto)ber)|(((sept|nov|dec)em)|okto)ber$");
+                    break;
+
+                case "Months":
+                    reg = new Regex(@"^(?:J(anuary|u(ne|ly))|February|Ma(rch|y)|A(pril|ugust)|(((Sept|Nov|Dec)em)|Octo)ber)$");
+                    break;
+
+                case "CurrencyDk":
+                    reg = new Regex(@"^[+-]?[0-9]{1,3}(?:.?[0-9]{3})*(?:\,[0-9]{2})?$");
+                    break;
+
+                case "Currency":
+                    reg = new Regex(@"^[+-]?[0-9]{1,3}(?:,?[0-9]{3})*(?:\.[0-9]{2})?$");
+                    break;
+
+                case "PhoneNumberDk":
+                    reg = new Regex(@"^(?:(?<local>(?:\d ?){8})|(?<international>\+(?:\d ?){10}))");
+                    break;
+
+                case "AddressDk":
+                    reg = new Regex(@"^(?:[A-zæøåÆØÅ]{2,40}\.?\s)+(?:[0-9]){1,5}\w?(?:\s.*)?$");
+                    break;
+
+                case "WholeNumbers":
+                    reg = new Regex(@"^\d+$");
+                    break;
+
+                case "DecimailComma":
+                    reg = new Regex(@"^\d*\,\d+$");
+                    break;
+
+                case "DecimalDot":
+                    reg = new Regex(@"^\d*\.\d+$");
+                    break;
+
+                case "Initials":
+                    reg = new Regex(@"(?<![A-Z])[A-Z]{2,5}(?![A-Z])");
+                    break;
+
+                case "XML":
+                    reg = new Regex(@"<(\w+)(\s(\w*="".*? "")?)*((/>)|((/*?)>.*?</\1>))");
                     break;
                 default:
-                    
+                    reg = null;
                     break;
             }
 
--- DeloitteValidation/MyEnum.cs
+++ DeloitteValidation/MyEnum.cs
@@ -3,14 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
 
 namespace DeloitteValidation
 {
     public enum MyEnum
     {
-        None,
+        [Display(Name = "Email Address")]
         Email,
-        OptionB,
-        OptionC
+        [Display(Name = "URL")]
+        Url,
+        [Display(Name = "IP Address")]
+        Ip,
+        [Display(Name = "Datetime (DK)")]
+        DatetimeDk,
+        [Display(Name = "Datetime")]
+        Datetime,
+        [Display(Name = "Days of the week (DK)")]
+        DaysoftheweekDk,
+        [Display(Name = "Days of the week")]
+        Daysoftheweek,
+        [Display(Name = "Months (DK)")]
+        MonthsDk,
+        [Display(Name = "Months")]
+        Months,
+        [Display(Name = "Currency (DK)")]
+        CurrencyDk,
+        [Display(Name = "Currency")]
+        Currency,
+        [Display(Name = "Phone number (DK)")]
+        PhoneNumberDk,
+        [Display(Name = "Address (DK)")]
+        AddressDk,
+        [Display(Name = "Whole Numbers")]
+        WholeNumbers,
+        [Display(Name = "Decimal Numbers (,)")]
+        DecimailComma,
+        [Display(Name = "Decimal Numbers (.)")]
+        DecimalDot,
+        [Display(Name = "Initials (2-5 Captial letters)")]
+        Initials,
+        [Display(Name = "XML Tag")]
+        XML
     }
 }
